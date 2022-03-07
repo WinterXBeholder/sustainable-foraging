601d14b5670132a7609f62e79b3b656cf55248aa
Brian <brian.hacker@ire.minnate.edu>
2022-03-07 03:58:59 -0600

    TODOs all DONE

--- src/main/java/learn/foraging/todo.java
+++ src/main/java/learn/foraging/todo.java
@@ -6,7 +6,7 @@ public class todo {
     //DONE: force add an item name to be a string, no numbers or punctuation
     //DONE: Add a Forager
     //DONE: Create a report that displays the kilograms of each Item collected on one day.
-    //TODO: Create a report that displays the total value of each Category collected on one day.
+    //DONE: Create a report that displays the total value of each Category collected on one day.
     //DONE: handle duplicate forages
     //DONE: implement Spring
 }
